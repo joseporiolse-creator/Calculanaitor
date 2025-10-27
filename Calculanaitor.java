3800a10e1b3675e5b9ed6db4ef602dfdb1217310
JordiCoso <jcosopa@alumnes.ilerna.com>
2025-10-27 20:06:32 +0100

    Parte 3 poner el codigo
    He colocado el codigo en el archivo

--- Calculanaitor.java
+++ Calculanaitor.java
@@ -3,7 +3,15 @@ package packet;
 public class Calculanaitor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("=== Calculadora Básica ===");
+	        int a = 10;
+	        int b = 5;
+	 
+	        System.out.println("Suma: " + (a + b));
+	        System.out.println("Resta: " + (a - b));
+	        System.out.println("Multiplicación: " + (a * b));
+	        System.out.println("División: " + (a / b));
 
 	}
 

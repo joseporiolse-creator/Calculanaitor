7cacb7f5d67cca199e11f7daf47cfcea49888b99
pandoras-g <erik.sesenya@alumnes.ilerna.com>
2025-10-28 22:07:34 +0100

    cambi de nom per el conflicte/agregat de percentatges
    cambi de nom mes la nova funcio de percentatges

--- src/Calculanaitor.java
+++ src/Calculanaitor.java
@@ -1,17 +1,17 @@
 
 public class Calculanaitor {
 
-	public static void main(String[] args) {
-		
-		System.out.println("=== Calculadora Básica ===");
-	        int a = 10;
-	        int b = 5;
-	 
-	        System.out.println("Suma: " + (a + b));
-	        System.out.println("Resta: " + (a - b));
-	        System.out.println("Multiplicación: " + (a * b));
-	        System.out.println("División: " + (a / b));
+    public static void main(String[] args) {
 
-	}
+        System.out.println("=== Calculadora rapida ===");
+        int a = 10;
+        int b = 5;
 
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicacion: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Potencia: " + Math.pow(a, b));
+        System.out.println("Porcentaje: " + ((a * b) / 100.0));
+    }
 }
